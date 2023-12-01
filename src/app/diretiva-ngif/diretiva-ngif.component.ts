5109f2fb4553096b59c802ddfdccb4c11f8c5178
Ericky2022 <erivaldo.pereira.santana2016@gmail.com>
2023-11-30 22:25:03 -0300

    Diretivas ngif, ngswitc e ngfor

--- src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
+  cursos: string[] = ["Angular 6"];
+
+  mostrarCursos: boolean = false;
+
   constructor(){}
 
   ngOnInit() {
 
   }
+
+onMostrarCursos(){
+  this.mostrarCursos = !this.mostrarCursos
+}
 
 }
